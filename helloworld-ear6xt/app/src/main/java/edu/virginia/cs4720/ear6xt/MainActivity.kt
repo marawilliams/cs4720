04bef3f31cbd26f21290d546cea1781cc2334244
marawilliams <mara.williams005@gmail.com>
2026-09-03 17:52:07 -0400

    finished with HW0

--- helloworld-ear6xt/app/src/main/java/edu/virginia/cs4720/ear6xt/MainActivity.kt
+++ helloworld-ear6xt/app/src/main/java/edu/virginia/cs4720/ear6xt/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import edu.virginia.cs4720.ear6xt.ui.theme.Helloworldear6xtTheme
@@ -19,10 +27,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Helloworldear6xtTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Greeting()
+                    }
                 }
             }
         }
@@ -30,17 +37,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    var name by remember {mutableStateOf("")}
+    var salutation by remember {mutableStateOf("Hello")}
+
+    if (name == ""){
+        Text(
+            text = "$salutation, Your name here!"
+        )
+    }
+    else {
+        Text(
+            text = "$salutation, $name!"
+        )
+    }
+
+    TextField( value = name, onValueChange = { name = it}, label = {Text("Name")})
+    Row() {
+        listOf("Hello", "Bonjour", "Ciao", "Ya Sou").forEach { word ->
+            Button(onClick = { salutation = word }) { Text("$word") }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Helloworldear6xtTheme {
-        Greeting("Android")
+        Column(){
+            Greeting()
+        }
     }
 }
