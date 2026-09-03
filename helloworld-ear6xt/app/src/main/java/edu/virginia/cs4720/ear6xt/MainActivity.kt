35533d020afc1ca9b4cef85a2521147e4dbc6d74
marawilliams <mara.williams005@gmail.com>
2026-09-03 13:43:45 -0400

    init

---
+++ helloworld-ear6xt/app/src/main/java/edu/virginia/cs4720/ear6xt/MainActivity.kt
@@ -0,0 +1,46 @@
+package edu.virginia.cs4720.ear6xt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.virginia.cs4720.ear6xt.ui.theme.Helloworldear6xtTheme
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Helloworldear6xtTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Helloworldear6xtTheme {
+        Greeting("Android")
+    }
+}
